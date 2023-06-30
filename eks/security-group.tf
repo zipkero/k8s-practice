faaf9d4370f816b2df0d2e2c804511f6526ecdd2
zipkero <zipkero@gmail.com>
2023-07-01 08:41:31 +0900

    modified sg inbound

--- eks/security-group.tf
+++ eks/security-group.tf
@@ -6,12 +6,21 @@ resource "aws_security_group" "eks-practice-cluster-sg" {
     }
 }
 
-resource "aws_security_group_rule" "eks-cluster-sg-ssh-inbound" {
+resource "aws_security_group_rule" "eks-cluster-sg-https-inbound" {
     security_group_id = aws_security_group.eks-practice-cluster-sg.id
     type = "ingress"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "eks-cluster-sg-http-inbound" {
+    security_group_id = aws_security_group.eks-practice-cluster-sg.id
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 }
 
